3d47c62f893efe97593b1bda273c20aa845281cd
MichalBeczek1985 <31369080+MichalBeczek1985@users.noreply.github.com>
2019-11-12 20:09:00 +0100

    adding hashcode and equals

--- src/main/java/com/majkel/springDemo/model/Author.java
+++ src/main/java/com/majkel/springDemo/model/Author.java
@@ -22,6 +22,31 @@ public class Author {
     public Author() {
     }
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
